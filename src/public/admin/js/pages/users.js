56e558cbcf466c108da87f2fea9916e18cb88fa9
Chuse <joseluis.latas@gmail.com>
2026-06-21 10:19:10 +0200

    Create users.js

---
+++ src/public/admin/js/pages/users.js
@@ -0,0 +1,14 @@
+Pages.users = function () {
+  document.getElementById('page').innerHTML = `
+    <div class="page-header">
+      <div>
+        <h1 class="page-title">Usuarios</h1>
+        <div class="page-subtitle">Gestión de administradores próximamente</div>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="empty">Módulo en construcción.</div>
+    </div>
+  `
+}
